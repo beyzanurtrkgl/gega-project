c02935fa426a5b091c07a99867afe9bd5cfde4c5
beyzanurtrkgl <beyzanurturkoglu06@gmail.com>
2023-12-10 12:13:48 +0300

    started file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+
+    container: {
+      center: true,
+    },
+    
+    extend: {
+      fontFamily:{
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-seri']
+      },
+
+      colors: {
+        "gega-red": "#BC1A45",
+        "gega-melon": "#FFD369",
+        "gega-grey": "#DDDDDD",
+        "gega-white": "#F7F7F7"
+      }
+    },
   },
   plugins: [],
 }
